4e6626d1c2b48945ca61789b96febec23326412c
Mayank Soni <202451104@iiitvadodara.ac.in>
2026-05-08 22:15:12 +0530

    link change

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT=process.env.PORT||2005;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://car-rental-m.vercel.app"
+    "https://car-rental-8ktp.onrender.com"
   ],
   credentials: true
 }));
